787aca139a36636a413006e88db049aa22bb2340
Ariori Abdulrafiu <abdulrafiuariori@gmail.com>
2023-11-12 02:37:56 -0800

    Continuation on the printf() creation

--- _printf.c
+++ _printf.c
@@ -1,5 +1,16 @@
 #include <stdarg.h>
 #include <stdio.h>
+
+/**
+ * _putchar - Creating a function that prints out char
+ * @c: char to be printed
+ * Return: int
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 /**
  * _printf- pring strings either formatted or not
  * @format: pointer to char (string) to be printed to the stdio
@@ -7,6 +18,14 @@
  * If % is found and the next char char after it is either i, d, c
  * ,s, it will have to map it to the variable passed to it after the %.
  * and then printf, sorry, print it to the stdout of the terminal.
+ *
+ * Some methods needed:
+ * # Helper function (just like the _putchar func created)
+ * # Recursion
+ * # Pointer to function (when necessary)
+ * # And other concept taught in C by ALX
+ * 
+ *
  * Return: classified as int.
  */
 int _printf(const char *format, ...)
